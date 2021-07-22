3985020c8914806a9a6e788ffffbad350d70897f
PVigar88 <Pvigar@gmail.com>
2021-07-21 22:47:32 -0400

    complete SQL queries, model, middleware

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,43 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Accounts = require("../accounts/accounts-model");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  Accounts.getAll()
+    .then((accounts) => {
+      res.status(200).json(accounts);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  Accounts.getById(req.params.id)
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.delete("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+});
 
 module.exports = router;
